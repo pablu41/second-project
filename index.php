0a15edd41cf6825fb15cc4e102551dffdfe25ce1
pablu41 <100160123+pablu41@users.noreply.github.com>
2022-03-24 19:38:48 +0600

    second assignment

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
 
       /*
 
@@ -40,16 +50,9 @@
       echo gettype($obj);
 
       $total = null; // datatype is NU
-       
-
-      
-
-
-
-
-
-
-
-
-
 ?>
+
+<?php>
+
+</body>
+</html>
